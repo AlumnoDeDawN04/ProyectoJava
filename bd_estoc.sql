837b7656ea855d11341efdae9bad4afef842c980
AlumnoDeDawN04 <55807836+AlumnoDeDawN04@users.noreply.github.com>
2020-02-18 20:02:52 +0100

    DB con precio

--- bd_estoc.sql
+++ bd_estoc.sql
@@ -1,144 +1,214 @@
-﻿SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-02-2020 a las 20:02:07
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `bd_estoc`
 --
-CREATE DATABASE IF NOT EXISTS `bd_estoc` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `bd_estoc`;
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `tbl_categoria`
+--
 
+CREATE TABLE `tbl_categoria` (
+  `categoria_id` int(11) NOT NULL,
+  `categoria_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_estoc`
+--
+
+CREATE TABLE `tbl_estoc` (
+  `estoc_id` int(11) NOT NULL,
+  `estoc_q_max` int(5) NOT NULL,
+  `estoc_q_min` int(5) NOT NULL,
+  `prod_id` int(11) DEFAULT NULL,
+  `lloc_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tbl_lloc`
+--
 
-CREATE TABLE IF NOT EXISTS `tbl_lloc` (
-  `lloc_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `tbl_lloc` (
+  `lloc_id` int(11) NOT NULL,
   `num_bloc` enum('Bloc 1','Bloc 2','Bloc3','Bloc 4','Bloc 5','Bloc 6','Bloc 7','Bloc 8') COLLATE utf8_unicode_ci NOT NULL,
   `num_passadis` enum('passadis 1','passadis 2','passadis 3','passadis 4','passadis 5','passadis 6','passadis 7','passadis 8','passadis 9','passadis 10','passadis 11','passadis 12') COLLATE utf8_unicode_ci NOT NULL,
   `num_lleixa` enum('lleixa 1','lleixa 2','lleixa 3','lleixa 4','lleixa 5','lleixa 6','lleixa 7','lleixa 8') COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
-
--- --------------------------------------------------------
--- Estructura de tabla para la tabla `tbl_estoc`
-
-CREATE TABLE IF NOT EXISTS `tbl_estoc` (
-  `estoc_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `estoc_q_max` int(5) NOT NULL,
-  `estoc_q_min` int(5) NOT NULL,
-  `prod_id` int(11)  NULL,
-  `lloc_id` int(11)  NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
 -- --------------------------------------------------------
 
--- Estructura de tabla para la tabla `tbl_proveidor`
-
-/*CREATE TABLE IF NOT EXISTS `tbl_proveidor` (
-  `prov_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `prov_nom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `prov_adre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `prov_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;*/
- 
- 
- 
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla `tbl_categoria` ----
-
-CREATE TABLE IF NOT EXISTS `tbl_categoria`(
-  `categoria_id` int(11) NOT NULL  PRIMARY KEY AUTO_INCREMENT,
-  `categoria_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla `tbl_serie`
-
-CREATE TABLE IF NOT EXISTS `tbl_serie` (
-  `serie_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `serie_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `categoria_id` int(11) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-
--- --------------------------------------------------------
+--
 -- Estructura de tabla para la tabla `tbl_producte`
-CREATE TABLE IF NOT EXISTS `tbl_producte` (
-  `prod_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--
+
+CREATE TABLE `tbl_producte` (
+  `prod_id` int(11) NOT NULL,
   `prod_nom` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `prod_foto` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `serie_id` int(11) NULL,
-  `prod_descripcio` varchar(100) COLLATE utf8_unicode_ci NULL,
-  `prod_descompte` int(3) NULL
+  `serie_id` int(11) DEFAULT NULL,
+  `prod_descripcio` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `prod_descompte` int(3) DEFAULT NULL,
+  `prod_preu` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `tbl_serie`
+--
+
+CREATE TABLE `tbl_serie` (
+  `serie_id` int(11) NOT NULL,
+  `serie_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `categoria_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
--- Estructura de tabla para la tabla `tbl_comanda`
-/*CREATE TABLE IF NOT EXISTS `tbl_comanda` (
-  `comanda_id` int(11) NOT NULL AUTO_INCREMENT,
-  `comanda_quantitat` int(5) NOT NULL,
-  `comanda_data` date NOT NULL,
-  `prov_id` int(11)  NULL,
-  PRIMARY KEY (`comanda_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;*/
 
+--
+-- Estructura de tabla para la tabla `tbl_usuario`
+--
 
--- --------------------------------------------------------
--- Estructura de tabla para la tabla `tbl_detall_comanda`
-/*CREATE TABLE IF NOT EXISTS `tbl_detall_com` (
-  `detall_com_id` int(11) NOT NULL AUTO_INCREMENT,
-  `comanda_id` int(11) NOT NULL,
-  `prod_id` int(11)  NULL,
-  PRIMARY KEY (`detall_com_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;*/
+CREATE TABLE `tbl_usuario` (
+  `id_user` int(1) NOT NULL,
+  `email_user` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password_user` text COLLATE utf8_unicode_ci NOT NULL,
+  `nombre_user` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Índices para tablas volcadas
+--
 
+--
+-- Indices de la tabla `tbl_categoria`
+--
+ALTER TABLE `tbl_categoria`
+  ADD PRIMARY KEY (`categoria_id`);
 
+--
+-- Indices de la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  ADD PRIMARY KEY (`estoc_id`),
+  ADD KEY `FK_estoc_prod` (`prod_id`),
+  ADD KEY `FK_estoc_lloc` (`lloc_id`);
 
--- -------------------------------------------------------
--- RELACIONES PARA LAS TABLAS 
--- ---------------------------------------------------
+--
+-- Indices de la tabla `tbl_lloc`
+--
+ALTER TABLE `tbl_lloc`
+  ADD PRIMARY KEY (`lloc_id`);
 
--- `tbl_serie` `tbl_categoria`:
-ALTER TABLE `tbl_serie` ADD CONSTRAINT `FK_serie_categoria` FOREIGN KEY (categoria_id)
-REFERENCES `tbl_categoria`(categoria_id);
--- ---------------------------------------------------
+--
+-- Indices de la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  ADD PRIMARY KEY (`prod_id`),
+  ADD KEY `FK_prod_serie` (`serie_id`);
 
--- `tbl_producte`  `tbl_serie`:
-ALTER TABLE `tbl_producte` ADD CONSTRAINT `FK_prod_serie` FOREIGN KEY (serie_id)
-REFERENCES `tbl_serie`(serie_id);
--- ---------------------------------------------------
-/*
--- `tbl_detall_comanda` `tbl_producte`:
-ALTER TABLE `tbl_detall_com` ADD CONSTRAINT `FK_detall_com_prod` FOREIGN KEY (prod_id)
-REFERENCES `tbl_producte`(prod_id);
--- ---------------------------------------------------
+--
+-- Indices de la tabla `tbl_serie`
+--
+ALTER TABLE `tbl_serie`
+  ADD PRIMARY KEY (`serie_id`),
+  ADD KEY `FK_serie_categoria` (`categoria_id`);
 
--- `tbl_detall_comanda` `tbl_comanda`:
-ALTER TABLE `tbl_detall_com` ADD CONSTRAINT `FK_detall_com_comanda` FOREIGN KEY (comanda_id)
-REFERENCES `tbl_comanda`(comanda_id);
--- ---------------------------------------------------
+--
+-- Indices de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  ADD PRIMARY KEY (`id_user`);
 
--- `tbl_comanda` `tbl_proveidor`:
-ALTER TABLE `tbl_comanda` ADD CONSTRAINT `FK_comanda_prov` FOREIGN KEY (prov_id)
-REFERENCES `tbl_proveidor`(prov_id);
--- ---------------------------------------------------
-*/
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
--- `tbl_estoc` `tbl_producte`:
-ALTER TABLE `tbl_estoc` ADD CONSTRAINT `FK_estoc_prod` FOREIGN KEY (prod_id)
-REFERENCES `tbl_producte`(prod_id);
--- ---------------------------------------------------
+--
+-- AUTO_INCREMENT de la tabla `tbl_categoria`
+--
+ALTER TABLE `tbl_categoria`
+  MODIFY `categoria_id` int(11) NOT NULL AUTO_INCREMENT;
 
--- `tbl_estoc `tbl_lloc`:
-ALTER TABLE `tbl_estoc` ADD CONSTRAINT `FK_estoc_lloc` FOREIGN KEY (lloc_id)
-REFERENCES `tbl_lloc`(lloc_id);
--- ----------------------------------------
+--
+-- AUTO_INCREMENT de la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  MODIFY `estoc_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_lloc`
+--
+ALTER TABLE `tbl_lloc`
+  MODIFY `lloc_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_serie`
+--
+ALTER TABLE `tbl_serie`
+  MODIFY `serie_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  MODIFY `id_user` int(1) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  ADD CONSTRAINT `FK_estoc_lloc` FOREIGN KEY (`lloc_id`) REFERENCES `tbl_lloc` (`lloc_id`),
+  ADD CONSTRAINT `FK_estoc_prod` FOREIGN KEY (`prod_id`) REFERENCES `tbl_producte` (`prod_id`);
+
+--
+-- Filtros para la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  ADD CONSTRAINT `FK_prod_serie` FOREIGN KEY (`serie_id`) REFERENCES `tbl_serie` (`serie_id`);
+
+--
+-- Filtros para la tabla `tbl_serie`
+--
+ALTER TABLE `tbl_serie`
+  ADD CONSTRAINT `FK_serie_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `tbl_categoria` (`categoria_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
